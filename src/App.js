dcfd30f1d6ece0564b6e859465e810d3fc0af633
Peter Daily <peter.daily@yahoo.co.uk>
2018-07-07 19:35:02 +0100

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        Hello
-      </div>
-    );
+    return <div className="App">Hello</div>
   }
 }
 
-export default App;
+export default App
