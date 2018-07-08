ea4f730352d442e09c20c9b415ba096a20c389b4
Peter Daily <peter.daily@yahoo.co.uk>
2018-07-08 11:45:00 +0100

    wait for redux persist before render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { createStore, compose, applyMiddleware } from "redux"
+import { offline } from "@redux-offline/redux-offline"
+import offlineConfig from "@redux-offline/redux-offline/lib/defaults"
+import thunk from "redux-thunk"
+import reducer from "./reducers"
+
 import "./index.css"
 import App from "./App"
-import { Provider } from "react-redux"
-import store from "./store/configureStore"
 
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+const render = () =>
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  )
+
+// await fetch of persisted store before render
+offlineConfig.persistCallback = render
+// configure redux store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk), offline(offlineConfig))
 )
+
 // registerServiceWorker();
