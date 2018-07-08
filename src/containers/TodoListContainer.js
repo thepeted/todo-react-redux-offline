297662df192a6d8ae017cf51da086c1ce622517c
Peter Daily <peter.daily@yahoo.co.uk>
2018-07-08 19:32:18 +0100

    take copy of todo items in container

--- src/containers/TodoListContainer.js
+++ src/containers/TodoListContainer.js
@@ -3,7 +3,7 @@ import TodoList from "../components/TodoList/TodoList"
 import { updateTodo, deleteTodo } from "../actions/todos"
 
 const mapStateToProps = ({ todos }) => ({
-  todos: todos.items.sort((a, b) => b.createdDate - a.createdDate)
+  todos: todos.items.slice(0).sort((a, b) => b.createdDate - a.createdDate)
 })
 
 const TodoListContainer = connect(
