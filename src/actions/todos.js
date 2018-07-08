afac1b77c2e30d2c1f248f17bffc4d9a8c2b01b4
Peter Daily <peter.daily@yahoo.co.uk>
2018-07-08 10:03:49 +0100

    generate todoId on post

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -1,16 +1,20 @@
 import { API_BASE } from "../constants"
+import { v1 as uuid } from "uuid"
 
-export const addTodo = (text, todoId = "temp") => ({
-  type: "POST_TODO_REQUEST",
-  payload: { text },
-  meta: {
-    offline: {
-      effect: {
-        url: `${API_BASE}/todos`,
-        method: "POST",
-        body: JSON.stringify({ text, completed: false })
-      },
-      commit: { type: "POST_TODO_COMMIT", meta: { todoId } }
+export const addTodo = text => {
+  const todoId = uuid()
+  return {
+    type: "POST_TODO_REQUEST",
+    payload: { text, todoId },
+    meta: {
+      offline: {
+        effect: {
+          url: `${API_BASE}/todos`,
+          method: "POST",
+          body: JSON.stringify({ text, completed: false, todoId })
+        },
+        commit: { type: "POST_TODO_COMMIT", meta: { todoId } }
+      }
     }
   }
-})
+}
